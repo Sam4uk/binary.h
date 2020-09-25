cb8343181c6ac3179740eafe4045b9f3531d1265
Sam4uk <sto.ukr.net@gmail.com>
2020-09-25 22:20:35 +0300

    binary.h

---
+++ OUT/binary.h
@@ -0,0 +1,36 @@
+//cut file
+/*
+* --------------------------------------------------------------------
+* binary.h - Definitions for binary constants
+* "THE BEER-WARE LICENSE":
+* <sto.ukr.net@gmail.com> wrote this file. As long as you retain this 
+* notice you can do whatever you want with this stuff. If we meet some
+* day, and you think this stuff is worth it, you can buy me a beer in 
+* return.
+* --------------------------------------------------------------------
+*/
+#ifndef BINARY__H
+  #define BINARY__H 1
+  #define B0 0x00
+  #define B00 0x00
+  #define B000 0x00
+  #define B0000 0x00
+  #define B00000 0x00
+  #define B000000 0x00
+  #define B0000000 0x00
+  #define B00000000 0x00
+  #define B1 0x01
+  #define B01 0x01
+  #define B001 0x01
+  #define B0001 0x01
+  #define B00001 0x01
+ /*
+ 
+      .   .   .
+ */
+  #define B11111011 0xfb
+  #define B11111100 0xfc
+  #define B11111101 0xfd
+  #define B11111110 0xfe
+  #define B11111111 0xff
+#endif
